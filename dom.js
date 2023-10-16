ed44cfb14e9a7753912172642188d1ee2a909079
markk87 <markk87@nycstudents.net>
2023-10-16 09:12:52 -0400

    bblkjlkjblb

--- dom.js
+++ dom.js
@@ -8,6 +8,7 @@ DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.firstName.value);
     DOMSelectors.h2.forEach((el) => el.textContent = DOMSelectors.firstName.value);
+    DOMSelectors.h2.forEach((el) => el.textContent = DOMSelectors.email.value);
 });
 
 
