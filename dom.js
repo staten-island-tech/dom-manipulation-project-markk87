0a16cfceed9946eca4193c480004ee0e476afad9
markk87 <markk87@nycstudents.net>
2023-10-17 09:23:22 -0400

    dvhgdfgdfg

--- dom.js
+++ dom.js
@@ -2,14 +2,16 @@ const DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
     h2: document.querySelectorAll("h2"),
+    emailInput: document.querySelector(".electro-mail"),
 };
 
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
-    DOMSelectors.h2.forEach((el) => el.textContent = DOMSelectors.firstName.value);
-    DOMSelectors.h2.forEach((el) => el.textContent = DOMSelectors.email.value);
+    console.log(DOMSelectors.firstName.value, DOMSelectors.emailInput.value);
+    DOMSelectors.h2.forEach((el) => el.textContent = DOMSelectors.firstName.value, DOMSelectors.emailInput.value);
 });
+
+
 
 
 
