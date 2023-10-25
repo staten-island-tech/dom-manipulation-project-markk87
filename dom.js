7637aa52aa549c2e84d48c0415d08a13ff9b0efe
markk87 <markk87@nycstudents.net>
2023-10-25 08:59:36 -0400

    sdfsdf

--- dom.js
+++ dom.js
@@ -1,59 +1,22 @@
 const DOMSelectors = {
     form: document.querySelector("#form"),
+    firstNameSubmit: document.querySelector("#submitname"),
     firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
     emailInput: document.querySelector(".electro-mail"),
+    gallery: document.querySelector('.gallery')
 };
 
-const HTMLSelectors = {
-    
+
+function createFart() {
+    DOMSelectors.gallery.insertAdjacentHTML('afterend', `<div class=card><h2> ${DOMSelectors.firstName.value} : ${DOMSelectors.emailInput.value}</h2></div>`)
 }
+DOMSelectors.form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('fart')
+    createFart();
+})
 
-function create(){
-    const Noah = {
-        firstName: DOMSelectors.firstName.value,
-        emailInput: DOMSelectors.emailInput.value,
-    };
-
-    inject(Noah);
-    clear();
-    remove();
-};
-
-function inject(Noah) {
-    HTMLSelectors.
-}
-
-
-
-const HTMLSelectors = {
-    
-}
-
-function createObject(name, email) {
-    return {
-        name,
-        email,
-    };
-}
-
-function addData() {
-    const name = DOMSelectors.nameInput.value;
-    const email = DOMSelectors.emailInput.value;
-}
-
-
-
-
-function createCard(){
-    const newCard = document.createElement("div");
-    newCard.appendChild();
-}
-
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-    
-});
 
 
 
