4e00373b7230253ee15a892c43c5ebab95444f61
markk87 <markk87@nycstudents.net>
2023-10-26 09:21:52 -0400

    sdfsdfasf

--- dom.js
+++ dom.js
@@ -2,21 +2,40 @@ const DOMSelectors = {
     form: document.querySelector("#form"),
     firstNameSubmit: document.querySelector("#submitname"),
     firstName: document.querySelector(".first-name"),
-    h2s: document.querySelectorAll("h2"),
     emailInput: document.querySelector(".electro-mail"),
     gallery: document.querySelector('.gallery'),
-    attributeInput: document.querySelector('#obtainAtt')
+    attributeInput: document.getElementById('obtainAtt')
 };
+
+
+
 
 
 
 DOMSelectors.form.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log('fart')
-    function createFart() {
-        DOMSelectors.gallery.insertAdjacentHTML('beforeend', `<div class=card><h2> ${DOMSelectors.firstName.value} : ${DOMSelectors.emailInput.value} : ${DOMSelectors.attributeInput.value} </h2></div>`)
+    console.log('fart');
+    function clearInputs() {
+        DOMSelectors.firstName.value = "";
+        DOMSelectors.emailInput.value = "";
+        DOMSelectors.attributeInput.value = "";
     }
+    function createFart() {
+        DOMSelectors.gallery.insertAdjacentHTML('beforeend', `<div class=card><button class=remove>Clear</button><h2> ${DOMSelectors.firstName.value} : ${DOMSelectors.emailInput.value} :  </h2><img class="imginput" src=${DOMSelectors.attributeInput.value}/></div>`)
+    }
+    function removeFart(){
+        const urmom = document.querySelectorAll(".remove");
+        urmom.forEach((el) => {
+            el.addEventListener('click', function(f){
+                f.currentTarget.parentNode.remove();
+            })
+        })
+     }
+    
     createFart();
+    removeFart();
+    clearInputs();
+
     console.log(DOMSelectors.gallery)
 })
 
